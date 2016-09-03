20adc66b60f91edbb5f8c2982f648809eb85c70b
bkjohnson <brooks.johnson88@gmail.com>
2016-09-02 20:51:15 -0500

    Adds sample biobots data to database.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,19 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { BiobotsData } from '../imports/api/biobotsdata.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    if (BiobotsData.find().count() === 0){
+
+        let data = JSON.parse(Assets.getText("bioprint-data.json"));
+
+        data.forEach(function (item, index, array) {
+            BiobotsData.insert(item);
+        });
+
+        console.log("Database populated");
+    }
+    else {
+        console.log("Database loaded");
+    }
 });
